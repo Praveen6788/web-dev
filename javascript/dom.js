7bd0acf135ac60eeff6bbdb177ae5cfe7e0c6435
Praveen6788 <22951a6786@iare.ac.in>
2024-09-20 19:48:33 +0530

    day 18

---
+++ javascript/dom.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+
+
+// cont.firstElementChild
+// <div class=​"box">​box 1​</div>​
+// cont.lastElementChild
+// <div class=​"box">​box 5​</div>​
+// cont.lastElementChild.st
+// undefined
+// cont.lastElementChild.style.backgroundColor = "red"
+// 'red'
+// cont.lastElementChild.parentElement
+// <div class=​"container">​…​</div>​
+// document.body.firstElementChild
+// <div class=​"container">​…​</div>​
+// document.body.firstElementChild.style.backgroundColor = "yello"
+// 'yello'
+// document.body.firstElementChild.childNodes
+
+
+
+// the code runs is in console
+
+let con = document.body.childNodes
